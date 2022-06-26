0a10deaf5b2d1cce230727fa3e2d9387017f5a1e
shehan <shehan@slatescience.com>
2022-06-27 00:08:59 +0530

    create MeetupItem & MeetupList components

---
+++ react-course/src/components/meetups/MeetupItem.js
@@ -0,0 +1,22 @@
+import classes from "./MeetupItem.module.css";
+
+function MeetupItem(props) {
+  const { title, image, address, description } = props.meetup;
+  return (
+    <li className={classes.item}>
+      <div className={classes.image}>
+        <img src={image} alt={title} />
+      </div>
+      <div className={classes.content}>
+        <h3>{title}</h3>
+        <address>{address}</address>
+        <p>{description}</p>
+      </div>
+      <div className={classes.actions}>
+        <button>To Favourites</button>
+      </div>
+    </li>
+  );
+}
+
+export default MeetupItem;
