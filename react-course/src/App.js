66e21bdaba0a26be98cc13fccef53f87f4c7476b
shehan <shehan@slatescience.com>
2022-06-26 17:26:18 +0530

    initial-commit

---
+++ react-course/src/App.js
@@ -0,0 +1,14 @@
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text="Learn ReactJs" />
+      <Todo text="Master ReactJs" />
+      <Todo text="Explore ReactJs" />
+    </div>
+  );
+}
+
+export default App;
