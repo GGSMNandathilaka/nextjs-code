778fed99ccbf5ac33d4b9657428d972fe5806b7e
shehan <shehan@slatescience.com>
2022-07-31 14:11:41 +0530

    navigate to the filtered events page programatical

--- nextjs-course/pages/events/index.js
+++ nextjs-course/pages/events/index.js
@@ -1,9 +1,31 @@
+import { Fragment } from "react";
+import { useRouter } from "next/router";
+import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
+import { getAllEvents } from "../../data/data";
+
 function AllEventsPage() {
+  const router = useRouter();
+  const allEvents = getAllEvents();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+
+    // load the slug path
+    router.push(fullPath);
+  }
+
   return (
     <div>
-      <h1>This is the events component</h1>
+      {allEvents && (
+        <Fragment>
+          <EventsSearch onSearch={findEventsHandler}></EventsSearch>
+          <EventList events={allEvents}></EventList>
+        </Fragment>
+      )}
     </div>
   );
 }
 
+<EventsSearch></EventsSearch>;
 export default AllEventsPage;
