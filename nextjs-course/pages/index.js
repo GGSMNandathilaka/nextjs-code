fec0c5753b6d791a21ceb088c516eb707ee4d5ad
shehan <shehan@slatescience.com>
2022-07-31 17:47:08 +0530

    getStaticProps implementation

--- nextjs-course/pages/index.js
+++ nextjs-course/pages/index.js
@@ -1,20 +1,29 @@
-import Link from "next/link";
+import fs from "fs/promises";
+import path from "path";
 
-function HomePage() {
+function HomePage(props) {
+  const { products } = props;
   return (
-    <div>
-      <h1>The HomePage</h1>
-
-      <ul>
-        <li>
-          <Link href="/portfolio">Portfolio</Link>
-        </li>
-        <li>
-          <Link href="/clients">Clients</Link>
-        </li>
-      </ul>
-    </div>
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
+    </ul>
   );
+}
+
+// executes in server-side
+// in the build time of this application
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json"); // absolute path to the file
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  return {
+    props: {
+      products: data.products,
+    },
+  };
 }
 
 export default HomePage;
--- nextjs-course/pages/portfolio/list.js
+++
@@ -1,9 +0,0 @@
-function PortfolioList() {
-  return (
-    <div>
-      <h1>Portfolio List Page</h1>
-    </div>
-  );
-}
-
-export default PortfolioList;
