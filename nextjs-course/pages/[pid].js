6061526da8f0ed4fe7e8c9b874b482204b4f9a54
shehan <shehan@slatescience.com>
2022-08-07 15:14:28 +0530

    getStaticPaths implementation

--- nextjs-course/pages/[pid].js
+++ nextjs-course/pages/[pid].js
@@ -4,6 +4,11 @@ import path from "path";
 
 function ProductDetailPage(props) {
   const { loadedProduct } = props;
+
+  if (!loadedProduct) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <Fragment>
       <h1>{loadedProduct.title}</h1>
@@ -12,15 +17,23 @@ function ProductDetailPage(props) {
   );
 }
 
+async function getData() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json"); // absolute path to the file
+  const jsonData = await fs.readFile(filePath);
+  return JSON.parse(jsonData);
+}
+
 export async function getStaticProps(context) {
   const { params } = context;
   const productId = params.pid;
 
-  const filePath = path.join(process.cwd(), "data", "dummy-backend.json"); // absolute path to the file
-  const jsonData = await fs.readFile(filePath);
-  const data = JSON.parse(jsonData);
+  const data = await getData();
 
   const product = data.products.find((product) => product.id === productId);
+
+  if (!product) {
+    return { notFound: true };
+  }
 
   return {
     props: {
@@ -30,14 +43,15 @@ export async function getStaticProps(context) {
 }
 
 export async function getStaticPaths() {
+  const data = await getData();
+  const paramList = data.products.map((product) => ({
+    params: {
+      pid: product.id,
+    },
+  }));
   return {
-    paths: [
-      { params: { pid: "p1" } },
-      { params: { pid: "p2" } },
-      { params: { pid: "p3" } },
-    ],
-    fallback: false,
+    paths: paramList,
+    fallback: true,
   };
 }
-
 export default ProductDetailPage;
--- nextjs-course/pages/index.js
+++ nextjs-course/pages/index.js
@@ -31,7 +31,7 @@ export async function getStaticProps() {
     props: {
       products: data.products,
     },
-    revalidate: 10,
+    revalidate: 10
   };
 }
 
