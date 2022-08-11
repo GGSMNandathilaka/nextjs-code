9f5ac1ee0d63a2cd132c882e695241f3becf5a79
shehan <shehan@slatescience.com>
2022-08-11 15:49:12 +0530

    combine pre-fetching with client-side fetching

--- nextjs-course/pages/last-sales.js
+++ nextjs-course/pages/last-sales.js
@@ -1,15 +1,13 @@
 import { useState, useEffect } from "react";
 import useSWR from "swr";
 
-function LastSales() {
+function LastSales(props) {
   const url =
     "https://nextjs-course-790d7-default-rtdb.firebaseio.com/sales.json";
-  const [sales, setSales] = useState();
+  const [sales, setSales] = useState(props.sales);
 
-  const { data, error } = useSWR(
-    url,
-    (url) => fetch(url).then((res) => res.json()),
-    { revalidateOnFocus: false }
+  const { data, error } = useSWR(url, (url) =>
+    fetch(url).then((res) => res.json())
   );
 
   useEffect(() => {
@@ -30,7 +28,7 @@ function LastSales() {
     return <p>Failed to load...</p>;
   }
 
-  if (!data || !sales) {
+  if (!data && !sales) {
     return <p>Loading...</p>;
   }
 
@@ -43,6 +41,32 @@ function LastSales() {
       ))}
     </ul>
   );
+}
+
+export async function getStaticProps() {
+  const response = await fetch(
+    "https://nextjs-course-790d7-default-rtdb.firebaseio.com/sales.json",
+    {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+    }
+  );
+  const data = await response.json();
+
+  const transformedSales = [];
+
+  for (const key in data) {
+    transformedSales.push({
+      id: key,
+      username: data[key].username,
+      volume: data[key].volume,
+    });
+  }
+
+  return { props: { sales: transformedSales } };
 }
 
 export default LastSales;
