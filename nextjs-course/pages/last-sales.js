bd4d273a8a255e10ea934b1af264cfb711a1036c
shehan <shehan@slatescience.com>
2022-08-14 19:43:55 +0530

    implement static generation for home page and detailed

---
+++ nextjs-course/pages/last-sales.js
@@ -0,0 +1,74 @@
+import { useState, useEffect } from "react";
+import useSWR from "swr";
+
+function LastSales(props) {
+  const url =
+    "https://nextjs-course-790d7-default-rtdb.firebaseio.com/sales.json";
+  const [sales, setSales] = useState(props.sales);
+
+  const { data, error } = useSWR(url, (url) =>
+    fetch(url).then((res) => res.json())
+  );
+
+  useEffect(() => {
+    if (data) {
+      const transformedSales = [];
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+      setSales(transformedSales);
+    }
+  }, [data]);
+
+  if (error) {
+    return <p>Failed to load...</p>;
+  }
+
+  if (!data && !sales) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <ul>
+      {sales.map((sale) => (
+        <li key={sale.id}>
+          {sale.username} - ${sale.volume}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export async function getStaticProps() {
+  const response = await fetch(
+    "https://nextjs-course-790d7-default-rtdb.firebaseio.com/sales.json",
+    {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+    }
+  );
+  const data = await response.json();
+
+  const transformedSales = [];
+
+  for (const key in data) {
+    transformedSales.push({
+      id: key,
+      username: data[key].username,
+      volume: data[key].volume,
+    });
+  }
+
+  return { props: { sales: transformedSales } };
+}
+
+export async function getServerSideProps() {}
+
+export default LastSales;
